7dfb0f0ef9ad2325e197bfc31747f318831a09b0
Toke Jepsen <tokejepsen@gmail.com>
2016-10-13 23:29:12 +0100

    check git existence

--- python_scripts/update.py
+++ python_scripts/update.py
@@ -1,7 +1,11 @@
 import subprocess
 
+import utils
+
 # install git and update
-subprocess.call(["conda", "install", "-c", "anaconda", "git", "-y"])
+if not utils.check_executable("git"):
+    subprocess.call(["conda", "install", "-c", "anaconda", "git", "-y"])
+
 subprocess.call(["git", "pull"])
 subprocess.call(["git", "submodule", "update", "--init", "--recursive"])
 subprocess.call(["git", "submodule", "update", "--recursive"])
