b751edb87b92257f8ba82622e7f03e6537fb8d2f
Toke Jepsen <tokejepsen@gmail.com>
2016-11-29 23:04:59 +0100

    Adding pyblsh-bumpybox and pyblish-ftrack

--- startup.py
+++ startup.py
@@ -1,79 +1,67 @@
 import os
 import subprocess
-import sys
+
 
 repo_root = os.path.join(os.path.dirname(__file__))
-
 env = {}
 
 # PYTHONPATH
-paths = [
+env["PYTHONPATH"] = [
     os.path.join(repo_root, "environment", "PYTHONPATH"),
-    os.path.join(os.environ["CONDA_GIT_REPOSITORY"], "pyblish-base"),
-    os.path.join(os.environ["CONDA_GIT_REPOSITORY"], "pyblish-lite"),
-    os.path.join(os.environ["CONDA_GIT_REPOSITORY"], "pyblish-maya"),
     os.path.join(os.environ["CONDA_GIT_REPOSITORY"], "pyblish-maya",
                  "pyblish_maya", "pythonpath"),
     os.path.join(os.environ["CONDA_GIT_REPOSITORY"], "pyblish-bumpybox"),
     os.path.join(os.environ["CONDA_GIT_REPOSITORY"], "pyblish-bumpybox",
                  "pyblish_bumpybox", "environment_variables", "pythonpath"),
 ]
-# Conda modifies the sys.path, so any application expecting python modules to
-# be in PYTHONPATH needs to be added manually. This is usually egg paths.
-for path in sys.path:
-    if path.endswith(".egg"):
-        paths.append(path)
-
-env["PYTHONPATH"] = paths
 
 # NUKE_PATH
-paths = [
+env["NUKE_PATH"] = [
     os.path.join(os.environ["CONDA_GIT_REPOSITORY"], "pyblish-bumpybox",
-                 "pyblish_bumpybox", "nuke_path"),
+                 "pyblish_bumpybox", "environment_variables", "nuke_path"),
 ]
 
-env["NUKE_PATH"] = paths
-
 # HOUDINI_PATH
-paths = [
+env["HOUDINI_PATH"] = [
     os.path.join(os.environ["CONDA_GIT_REPOSITORY"], "pyblish-bumpybox",
-                 "pyblish_bumpybox", "houdini_path"),
+                 "pyblish_bumpybox", "environment_variables", "houdini_path"),
     "&"
 ]
 
-env["HOUDINI_PATH"] = paths
-
 # HIERO_PLUGIN_PATH
-paths = [
+env["HIERO_PLUGIN_PATH"] = [
     os.path.join(os.environ["CONDA_GIT_REPOSITORY"], "pyblish-bumpybox",
-                 "pyblish_bumpybox", "hiero_plugin_path"),
+                 "pyblish_bumpybox", "environment_variables",
+                 "hiero_plugin_path"),
 ]
 
-env["HIERO_PLUGIN_PATH"] = paths
-
 # PYBLISHPLUGINPATH
-paths = [
+env["PYBLISHPLUGINPATH"] = [
     os.path.join(os.environ["CONDA_GIT_REPOSITORY"], "pyblish-bumpybox",
                  "pyblish_bumpybox", "plugins"),
     os.path.join(os.environ["CONDA_GIT_REPOSITORY"], "pyblish-bumpybox",
                  "pyblish_bumpybox", "plugins", "maya"),
     os.path.join(os.environ["CONDA_GIT_REPOSITORY"], "pyblish-bumpybox",
                  "pyblish_bumpybox", "plugins", "houdini"),
+    os.path.join(os.environ["CONDA_GIT_REPOSITORY"], "pyblish-bumpybox",
+                 "pyblish_bumpybox", "plugins", "ftrack"),
+    os.path.join(os.environ["CONDA_GIT_REPOSITORY"], "pyblish-ftrack",
+                 "pyblish_ftrack", "plugins"),
 ]
-
-env["PYBLISHPLUGINPATH"] = paths
 
 # FTRACK_CONNECT_PLUGIN_PATH
-# Adding hook directories for ftrack-connect to pick up actions.
-import ftrack_connect_maya
-
-ftrack_connect_plugin_path = [
-    os.path.abspath(
-        os.path.join(os.path.dirname(ftrack_connect_maya.__file__), "..")
-    ),
+env["FTRACK_CONNECT_PLUGIN_PATH"] = [
+    os.path.join(os.environ["CONDA_GIT_REPOSITORY"], "ftrack-connect-maya"),
     os.path.join(repo_root, "environment", "FTRACK_CONNECT_PLUGIN_PATH"),
+    #os.path.join(os.environ["CONDA_GIT_REPOSITORY"], "ftrack-hooks"),
 ]
-env["FTRACK_CONNECT_PLUGIN_PATH"] = ftrack_connect_plugin_path
+
+# FTRACK_CONNECT_MAYA_PLUGINS_PATH
+env["FTRACK_CONNECT_MAYA_PLUGINS_PATH"] = [
+    os.path.join(
+        os.environ["CONDA_GIT_REPOSITORY"], "ftrack-connect-maya", "resource"
+    )
+]
 
 # Setting environment.
 for variable in env:
