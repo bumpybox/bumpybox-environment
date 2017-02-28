583d26a326af941bc817a334c6de5fcbfb5e317e
Toke Jepsen <tokejepsen@gmail.com>
2017-02-28 13:30:40 +0100

    Deadline: Add group on Maya submisson.

---
+++ environment/PYBLISHPLUGINPATH/deadline/extract_maya.py
@@ -0,0 +1,19 @@
+import pyblish.api as api
+
+
+class BumpyboxEnvironmentDeadlineExtractMaya(api.InstancePlugin):
+    """ Add groups to Maya Submissions. """
+
+    order = api.ExtractorOrder
+    label = "Maya"
+    families = ["deadline"]
+
+    def process(self, instance):
+
+        data = instance.data.get(
+            "deadlineData", {"job": {}, "plugin": {}}
+        )
+
+        data["job"]["Group"] = "mtoa"
+
+        instance.data["deadlineData"] = data
