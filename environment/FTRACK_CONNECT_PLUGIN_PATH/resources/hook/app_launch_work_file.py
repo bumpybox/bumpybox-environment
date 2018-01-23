a98cd588475e99900e2165e00d54b6c51f20353b
Toke Jepsen <tokejepsen@gmail.com>
2018-01-23 23:54:27 +0100

    Launch NukeStudio work file.

--- environment/FTRACK_CONNECT_PLUGIN_PATH/resources/hook/app_launch_work_file.py
+++ environment/FTRACK_CONNECT_PLUGIN_PATH/resources/hook/app_launch_work_file.py
@@ -49,7 +49,7 @@ def get_task_data(event):
     if identifier.startswith("nukex"):
         app_id = "nuke"
     if identifier.startswith("nuke_studio"):
-        return
+        app_id = "nukestudio"
 
     # Maya
     if identifier.startswith("maya"):
